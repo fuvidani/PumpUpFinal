f12b93da2ef40af4dcb04e7f24a466e140029bf5
lukas <e1325082@student.tuwien.ac.at>
2015-04-30 18:22:59 +0200

    create_exercise added

---
+++ PumpUp/src/main/resources/create_exercise.sql
@@ -0,0 +1,21 @@
+
+
+create table exercise(
+id int not null auto_increment,
+name varchar not null,
+descripion varchar not null,
+calories double not null,
+videolink varchar,
+PRIMARY KEY(id)
+);
+
+create table gif(
+id int not null auto_increment,
+exerciseid int not null,
+location varchar not null,
+PRIMARY KEY(id),
+FOREIGN KEY(exerciseid) references exercise(id)
+);
+
+drop table exercise;
+drop table gif;
