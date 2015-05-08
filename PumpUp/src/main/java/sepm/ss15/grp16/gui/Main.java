6d5ac73c68b447e806631c0b458004398648753d
Lukas Baronyai <lukas.baronyai@gmail.com>
2015-05-08 15:43:45 +0200

    Logger for H2DBConnectorImpl & Main

--- PumpUp/src/main/java/sepm/ss15/grp16/gui/Main.java
+++ PumpUp/src/main/java/sepm/ss15/grp16/gui/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.util.Callback;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import sepm.ss15.grp16.gui.controller.ExercisesController;
@@ -16,9 +18,11 @@ import sepm.ss15.grp16.gui.controller.ExercisesController;
  * Edited by Daniel Fuevesi
  */
 public class Main extends Application{
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
 
 
     public void start(final Stage primaryStage) throws Exception {
+        LOGGER.info("starting application");
 
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
         FXMLLoader fxmlLoader = new FXMLLoader();
@@ -30,6 +34,8 @@ public class Main extends Application{
                 return context.getBean(clazz);
             }
         });
+
+        LOGGER.info("configuration successful");
 
         Pane pane = (Pane) fxmlLoader.load(ExercisesController.class.getClassLoader().getResourceAsStream("fxml/Exercises.fxml"));
         ExercisesController exercisesController = fxmlLoader.getController();
