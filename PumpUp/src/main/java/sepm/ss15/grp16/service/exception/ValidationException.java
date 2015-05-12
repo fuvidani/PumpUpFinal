0da3207de7cf6f39b90afd1cc482520b2230b198
Maximilian Medetz <maximilian.medetz@gmail.com>
2015-05-12 14:28:15 +0200

    JavaDoc in DAO und Service angepasst. ValidationException auf eigene Message angepasst.

--- PumpUp/src/main/java/sepm/ss15/grp16/service/exception/ValidationException.java
+++ PumpUp/src/main/java/sepm/ss15/grp16/service/exception/ValidationException.java
@@ -1,25 +1,41 @@
 package sepm.ss15.grp16.service.exception;
 
+import sepm.ss15.grp16.entity.DTO;
+
 /**
  * Created by lukas on 02.05.2015.
  */
 public class ValidationException extends ServiceException{
-    public ValidationException() {
+
+    private final String validationMessage;
+    private final DTO validatedDTO;
+
+    /**
+     * Creates a ValidationException
+     * @param validationMessage Message that should be displayed, after not valid input.
+     */
+    public ValidationException(String validationMessage) {
+        super("Couldn't validate dto.");
+        this.validationMessage = validationMessage;
+        this.validatedDTO = null;
     }
 
-    public ValidationException(String message) {
-        super(message);
+    /**
+     * Creates a ValidationException
+     * @param validationMessage Message that should be displayed, after not valid input.
+     * @param dto The dto which couldn't be validated.
+     */
+    public ValidationException(String validationMessage, DTO dto) {
+        super("Couldn't validate dto: " + dto);
+        this.validationMessage = validationMessage;
+        this.validatedDTO = dto;
     }
 
-    public ValidationException(String message, Throwable cause) {
-        super(message, cause);
+    public String getValidationMessage() {
+        return validationMessage;
     }
 
-    public ValidationException(Throwable cause) {
-        super(cause);
-    }
-
-    public ValidationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
+    public DTO getValidatedDTO() {
+        return validatedDTO;
     }
 }
