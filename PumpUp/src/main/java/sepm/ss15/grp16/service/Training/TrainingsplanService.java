d8b32179854a5747bf307e6b156e830fe0336551
Lukas Baronyai <lukas.baronyai@gmail.com>
2015-05-13 18:19:28 +0200

    finished TrainingsplanService & corresponding JUnits

--- PumpUp/src/main/java/sepm/ss15/grp16/service/Training/TrainingsplanService.java
+++ PumpUp/src/main/java/sepm/ss15/grp16/service/Training/TrainingsplanService.java
@@ -14,14 +14,50 @@ import java.util.List;
  */
 public interface TrainingsplanService extends Service<Trainingsplan> {
 
-	List<Trainingsplan> find(Trainingsplan trainingsplan) throws ServiceException;
+	/**
+	 * find all with filter matching trainingsplans
+	 *
+	 * @param filter for the search, must not be valid, you can search after name and user
+	 * @return found trainingsplan, null if nothing was found
+	 * @throws ServiceException fthere are complications with the service layer
+	 */
+	List<Trainingsplan> find(Trainingsplan filter) throws ServiceException;
 
+	/**
+	 * generates the corresponding trainingsplan for a trainingssession
+	 *
+	 * @param session must has a valid ID
+	 * @return the corresponding plan, null if nothing was found
+	 * @throws ServiceException fthere are complications with the service layer
+	 */
 	Trainingsplan getPlanBySession(TrainingsSession session) throws ServiceException;
 
+	/**
+	 * generates the corresponding trainingsplan for an ExerciseSet
+	 *
+	 * @param set must has a valid ID
+	 * @return the corresponding plan, null if nothing was found
+	 * @throws ServiceException fthere are complications with the service layer
+	 */
 	Trainingsplan getPlanBySet(ExerciseSet set) throws ServiceException;
+
+	/**
+	 * find all trainingssession of an user
+	 *
+	 * @param user to search, can be null, UID must not be null
+	 * @return found sessions, null if nothing was found
+	 * @throws ServiceException fthere are complications with the service layer
+	 */
 
 	List<TrainingsSession> searchByUser(User user) throws ServiceException;
 
+	/**
+	 * find all trainingssession of a corresponding plan
+	 *
+	 * @param ID_plan to search, must not be null
+	 * @return found sessions, null if nothing was found
+	 * @throws ServiceException fthere are complications with the service layer
+	 */
 	List<TrainingsSession> searchByPlanID(int ID_plan) throws ServiceException;
 
 }
