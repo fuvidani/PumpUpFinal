140ebcff73b77a97a4b8f8de83e627bb8d1b6eb4
Maximilian Medetz <maximilian.medetz@gmail.com>
2015-05-20 11:00:17 +0200

    Feature #43239 LiveMode fehler behoben.

--- PumpUp/src/main/java/sepm/ss15/grp16/gui/controller/Workout/WorkoutController.java
+++ PumpUp/src/main/java/sepm/ss15/grp16/gui/controller/Workout/WorkoutController.java
@@ -9,17 +9,22 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Arc;
-import javafx.scene.shape.Circle;
 import javafx.util.Duration;
-import sepm.ss15.grp16.entity.Training.Helper.ExerciseSet;
+import sepm.ss15.grp16.entity.Exercise;
+import sepm.ss15.grp16.entity.training.TrainingsSession;
+import sepm.ss15.grp16.entity.training.helper.ExerciseSet;
 import sepm.ss15.grp16.gui.controller.Controller;
+import sepm.ss15.grp16.service.ExerciseService;
+import sepm.ss15.grp16.service.Service;
+import sepm.ss15.grp16.service.exception.ServiceException;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
 
 /**
@@ -48,21 +53,45 @@ public class WorkoutController extends Controller implements Initializable {
     @FXML
     private Label counterLable;
 
-    private final Integer STARTTIME = 15;
-
     private Timeline timeline;
-    private IntegerProperty timeSeconds =
-            new SimpleIntegerProperty(STARTTIME);
+    private IntegerProperty timeSeconds;
 
-    ExerciseSet activeExercise;
+    ArrayList<ExerciseSet> exerciseList;
+    private int activeExercisePosition;
 
-    private boolean mode = true; // true counter mode
     private boolean pause = true;
 
+    TrainingsSession session;
 
-
+    public WorkoutController(ExerciseService exerciseService)
+    {
+        LinkedList<ExerciseSet> list = new LinkedList<>();
+        /*try {
+            int i = 10;
+            boolean mode = true;
+            for(Exercise exercise : exerciseService.getAllStrengthExercises())
+            {
+                ExerciseSet set = new ExerciseSet();
+                set.setExercise(exercise);
+                set.setType(new ExerciseSet.SetType(mode ? ExerciseSet.SetType.REPEAT : ExerciseSet.SetType.TIME));
+                list.add(new ExerciseSet());
+                i = i + 10;
+                mode = !mode;
+            }
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }*/
+        list.add(new ExerciseSet(null, null, 10, new ExerciseSet.SetType(ExerciseSet.SetType.REPEAT)))
+        session = new TrainingsSession();
+        session.setExerciseSets(list);
+    }
 
     public void initialize(URL location, ResourceBundle resources) {
+
+        exerciseList = new ArrayList<ExerciseSet>(session.getExerciseSets());
+        activeExercisePosition = 0;
+
+        timeSeconds = new SimpleIntegerProperty();
         counterLable.textProperty().bind(timeSeconds.asString());
         circleCounter.setLength(0);
 
@@ -76,36 +105,49 @@ public class WorkoutController extends Controller implements Initializable {
         pause();
     }
 
-    public void start(boolean mode) {
+    private ExerciseSet activeExercise()
+    {
+        return exerciseList.get(activeExercisePosition);
+    }
+
+    private void switchToNextExercise()
+    {
+        activeExercisePosition++;
+    }
+
+    private void runExercise() {
         pause = false;
         pauseButton.setText("Stop");
-        circleCounter.setLength(0);
-        timeline.getKeyFrames().clear();
-        if (mode) {
-            timeSeconds.set(STARTTIME);
-            timeline.getKeyFrames().add(
-                    new KeyFrame(Duration.seconds(STARTTIME + 1),
-                            new KeyValue(timeSeconds, 0), new KeyValue(circleCounter.lengthProperty(), 360)));
+        timeline.playFromStart();
+    }
 
+
+
+    private void pause() {
+        pause = true;
+        pauseButton.setText("Start");
+        timeline.getKeyFrames().clear();
+
+        switchToNextExercise();
+
+        if (activeExercise().getType().equals("time")) {
+            timeSeconds.set(activeExercise().getRepeat());
+            circleCounter.setLength(360);
+            timeline.getKeyFrames().add(
+                    new KeyFrame(Duration.seconds(activeExercise().getRepeat() + 1),
+                            new KeyValue(timeSeconds, 0), new KeyValue(circleCounter.lengthProperty(), 0)));
         } else {
             timeSeconds.set(0);
             timeline.getKeyFrames().add(
                     new KeyFrame(Duration.seconds(Integer.MAX_VALUE),
                             new KeyValue(timeSeconds, Integer.MAX_VALUE)));
         }
-        timeline.playFromStart();
-    }
-
-    public void pause() {
-        pause = true;
-        pauseButton.setText("Start");
     }
 
     @FXML
-    void onPause(ActionEvent event) {
+    private void onPause(ActionEvent event) {
         if (pause) {
-            start(mode);
-            mode = !mode;
+            runExercise();
         } else {
             timeline.stop();
             pause();
