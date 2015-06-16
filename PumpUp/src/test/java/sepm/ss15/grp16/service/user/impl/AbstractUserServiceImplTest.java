7c1b77b36515711aac41e01b7c89892d85f19f2b
Michael Peter Sober <michael.sober@ymail.com>
2015-06-17 00:16:28 +0200

    spring-config-test erstellt. Generische Servicetests mit Mockito erstellt. UserService-Test angepasst

--- PumpUp/src/test/java/sepm/ss15/grp16/service/user/impl/AbstractUserServiceImplTest.java
+++ PumpUp/src/test/java/sepm/ss15/grp16/service/user/impl/AbstractUserServiceImplTest.java
@@ -1,15 +1,15 @@
 package sepm.ss15.grp16.service.user.impl;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import sepm.ss15.grp16.persistence.database.DBHandler;
+import sepm.ss15.grp16.persistence.dao.user.UserDAO;
 import sepm.ss15.grp16.service.user.AbstractUserServiceTest;
 import sepm.ss15.grp16.service.user.UserService;
 
@@ -17,28 +17,24 @@ import sepm.ss15.grp16.service.user.UserService;
  * Created by michaelsober on 05.05.15.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:spring-config.xml")
+@ContextConfiguration("classpath:spring-config-test.xml")
 @TestExecutionListeners(inheritListeners = false, listeners =
         {DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class})
 public class AbstractUserServiceImplTest extends AbstractUserServiceTest {
-
-    @Autowired
-    private DBHandler dbConnector;
 
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
     }
 
-    @Before
-    public void setUp() throws Exception {
-        dbConnector.getConnection().setAutoCommit(false);
+    @Autowired
+    public void setMockedUserDAO(UserDAO mockedUserDAO) {
+        this.mockedUserDAO = mockedUserDAO;
     }
 
-    @After
-    public void tearDown() throws Exception {
-        dbConnector.getConnection().rollback();
-        dbConnector.getConnection().setAutoCommit(true);
+    @Before
+    public void setUp() throws Exception {
+        Mockito.reset(mockedUserDAO);
     }
 
 }
